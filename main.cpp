af7ea8e702de29ecc53a1de47d72fe9d0e194652
bilkitg <lg___gl@outlook.com>
2018-03-04 22:40:34 -0800

    Ensure loaded data is valid.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,20 @@ int main()
 	try
 	{
 		cv::Mat lenaImage = cv::imread("./SampleImages/lena512.bmp", CV_LOAD_IMAGE_GRAYSCALE);
-		std::cout << "Successfully loaded image!\n";
+		if (lenaImage.data)
+		{
+			std::cout << "Successfully loaded image!\n";
+		}
+		else
+		{
+			std::cout << "Failed to load image!\n	";
+			return -1;
+		}
 	}
 	catch (const std::exception& e)
 	{
-		std::cout << "Failed to load image!\n	";
+		std::cout << "Failed to load image due to exception!\n	";
+		return -1;
 	}
 
 	return 0;
