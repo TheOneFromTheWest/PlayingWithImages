8b38a30ced11e966c35bec76e432bbb2ab3857d3
bilkitg <lg___gl@outlook.com>
2018-03-04 22:30:20 -0800

    Successfully tested image loading.

--- main.cpp
+++ main.cpp
@@ -5,16 +5,15 @@ int main()
 {
 	std::cout << "Hi\n";
 	std::cout << "Attempting to load image\n";
-	bool isImageLoaded = false;
-
-
-	if (isImageLoaded)
+	try
 	{
+		cv::Mat lenaImage = cv::imread("./SampleImages/lena512.bmp", CV_LOAD_IMAGE_GRAYSCALE);
 		std::cout << "Successfully loaded image!\n";
 	}
-	else
+	catch (const std::exception& e)
 	{
-		std::cout << "Failed to load image!\n";
+		std::cout << "Failed to load image!\n	";
 	}
+
 	return 0;
 }
