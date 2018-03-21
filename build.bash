c57441afa183d5dfd8128669ee0cd8b3ee3cfdf5
bilkitg <lg___gl@outlook.com>
2018-03-20 18:09:04 -0700

    Ensure compiler is using c++11

--- build.bash
+++ build.bash
@@ -1,10 +1,15 @@
 #!bin/bash
 
-echo "compiling $1"
-if [[ $1 == *.cpp ]]
+if [[ $# < 1 ]]
 then
-    g++ -ggdb `pkg-config --cflags opencv` -o bin/`basename $1 .cpp` $1 LoadImage.* `pkg-config --libs opencv`;
+  echo "Usage:build.bash <MainCpp> "
 else
-    echo "Please compile .cpp files"
+  echo "compiling $1"
+  if [[ $1 == *.cpp ]]
+  then
+      g++ -ggdb `pkg-config --cflags opencv` -o bin/`basename $1 .cpp` $1 LoadImage.* `pkg-config --libs opencv` -std=c++11;
+  else
+      echo "Please compile .cpp files"
+  fi
+  echo "Output file => ${1%.*}"
 fi
-echo "Output file => ${1%.*}"
