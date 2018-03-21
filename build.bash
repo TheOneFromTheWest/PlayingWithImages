a6c6365c62317cdac5136b2d8b0fccc80013515e
bilkitg <lg___gl@outlook.com>
2018-03-21 16:36:42 -0700

    Remove requirement for args to be .cpp files.

--- build.bash
+++ build.bash
@@ -5,11 +5,6 @@ then
   echo "Usage:build.bash <MainCpp> "
 else
   echo "compiling $1"
-  if [[ $1 == *.cpp ]]
-  then
-      g++ -ggdb `pkg-config --cflags opencv` -o bin/$1 *.cpp *.h `pkg-config --libs opencv` -std=c++11;
-  else
-      echo "Please compile .cpp files"
-  fi
+g++ -ggdb `pkg-config --cflags opencv` -o bin/$1 *.cpp *.h `pkg-config --libs opencv` -std=c++11;
   echo "Output file => ${1%.*}"
 fi
