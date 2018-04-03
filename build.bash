6fed8091ea7ba5cc5156c031dc7f63500dc8402a
bilkitg <lg___gl@outlook.com>
2018-04-02 20:51:17 -0700

    Print compiler command.

--- build.bash
+++ build.bash
@@ -5,6 +5,7 @@ then
   echo "Usage:build.bash <MainCpp> "
 else
   echo "compiling $1"
+  echo "g++ -ggdb `pkg-config --cflags opencv` -o bin/$1 *.cpp *.h `pkg-config --libs opencv` -std=c++11"
 g++ -ggdb `pkg-config --cflags opencv` -o bin/$1 *.cpp *.h `pkg-config --libs opencv` -std=c++11;
   echo "Output file => ${1%.*}"
 fi
