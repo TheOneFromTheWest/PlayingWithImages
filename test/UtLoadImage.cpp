19745c3986ff2902fa3d61439c3af6c2ecd4d038
bilkitg <lg___gl@outlook.com>
2018-04-02 20:52:38 -0700

    Adding unit tests outline.

---
+++ test/UtLoadImage.cpp
@@ -0,0 +1,31 @@
+#include "../LoadImage.h" 
+#include "../ImageHelper.h"
+#include <gtest/gtest.h>
+
+// ^^ should these paths be relative paths?
+
+int main()
+{
+	/*
+	std::cout << "Hi\n";
+	ImageLoader imLoader;
+	std::shared_ptr<cv::Mat> pLenaImage = imLoader.Load("./SampleImages/lena512.bmp");
+	if (!pLenaImage->data)
+	{
+		std::cout << "Failed to load lena512!\n";
+		return 1;
+	}
+	else
+	{
+		std::cout << "Loaded lena512!\n";
+	}
+
+	ImageIllustrator imHelper;
+	imHelper.ShowImage("Lena!", pLenaImage);	
+	*/
+
+	printf("Running tests:\n");
+
+
+	return 0;
+}
