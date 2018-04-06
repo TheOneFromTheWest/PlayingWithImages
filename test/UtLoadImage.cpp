d0aa576a4263b54b9ebea73c53293176e9ab5073
bilkitg <lg___gl@outlook.com>
2018-04-05 21:10:28 -0700

    Add base and simple test cases for ImageLoader Load function.

--- test/UtLoadImage.cpp
+++ test/UtLoadImage.cpp
@@ -2,30 +2,44 @@
 #include "../ImageHelper.h"
 #include <gtest/gtest.h>
 
-// ^^ should these paths be relative paths?
 
-int main()
+namespace 
 {
-	/*
-	std::cout << "Hi\n";
-	ImageLoader imLoader;
-	std::shared_ptr<cv::Mat> pLenaImage = imLoader.Load("./SampleImages/lena512.bmp");
-	if (!pLenaImage->data)
+	// Simple tests
+	TEST(LoadImage, Simple)
 	{
-		std::cout << "Failed to load lena512!\n";
-		return 1;
-	}
-	else
-	{
-		std::cout << "Loaded lena512!\n";
+		// Cases 
+		std::string relativeImagePath = "./data/testImage.bmp";
+		std::string nonExistentImagepath = "./data/nonExistentImage.bmp";
+		std::string spacesImagePath = "./data/test Image.bmp";
+
+		ImageLoader iml;
+		EXPECT_EQ(false, iml.Load(relativeImagePath) == nullptr);
+		EXPECT_EQ(true, iml.Load(nonExistentImagepath) == nullptr);	
+		EXPECT_EQ(true, iml.Load(spacesImagePath) == nullptr);
 	}
 
-	ImageIllustrator imHelper;
-	imHelper.ShowImage("Lena!", pLenaImage);	
-	*/
+	// The basic tests 
+	TEST(LoadImage, Base)
+	{
+		std::string emptyImagePath = "";
+		std::string directoryPath = "./data";
+		std::string textFilePath = "./data/test.txt";
+		// Why don't absolute image paths work?
+		std::string absImagePath = "~/Workspace/Learn4Fun/ImageStitching/test/data/testImage.bmp";
 
+		ImageLoader iml;	
+		EXPECT_EQ(true, iml.Load(emptyImagePath) == nullptr);
+		EXPECT_EQ(true, iml.Load(directoryPath) == nullptr);
+		EXPECT_EQ(true, iml.Load(textFilePath) == nullptr);
+		EXPECT_EQ(true, iml.Load(absImagePath) == nullptr);
+	}
+
+} // namespace
+
+GTEST_API_ int main(int argc, char** argv)
+{
 	printf("Running tests:\n");
-
-
-	return 0;
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
